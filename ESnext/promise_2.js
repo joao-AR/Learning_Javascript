8023ab46090adc8aad1f6a8b85d32fe8ed1e21d9
Joao <jp.alvesrodrigues02@gmail.com>
2022-02-04 10:33:12 -0300

    add ES

--- ESnext/promise_2.js
+++ ESnext/promise_2.js
@@ -2,9 +2,7 @@ const primeiro_elemento = array_ou_string => array_ou_string[0]
 const letra_minuscula = letra => letra.toLowerCase()
 
 new Promise(function(resolve){
-
-    resolve (['Ana','Bia','Carlos','Daniel'])
-
+resolve (['Ana','Bia','Carlos','Daniel'])
 })
     .then (primeiro_elemento)
     .then (primeiro_elemento)
