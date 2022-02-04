788c56d69b6ca292cc272a657a358ca790812775
João Pedro <75582165+joao-AR@users.noreply.github.com>
2022-02-04 15:35:51 -0300

    Delete fundamentos directory

--- fundamentos/hoisting.js
+++
@@ -1,5 +0,0 @@
-console.log ('a = ', a) // essa linha não causa erro e JS pois quando ultiza "VAR" a declaração da variavel e mandada para cima 
-
- var a = 2  // se fizermos a mesmo coisa com "LET" isso vai dar erro
-
-console.log ('a = ', a)
