408e7f617d71a4ce39dc3c49ebe9b200593de141
João Pedro <75582165+joao-AR@users.noreply.github.com>
2022-02-04 15:35:41 -0300

    Delete funcoes directory

--- funcoes/iife.js
+++
@@ -1,7 +0,0 @@
-// IIFE -> immediately invoked function expression
-
-(function(){ // tudo que for criado aqui só pode ser ultilizado aqui dentro
-    
-    console.log ('será executado na hora')
-    console.log ("Foge do escopo mais abrangete ")
-}) ()
