13a9c8b5d5b2d7eff1637c4aadfa2149ea9263df
João Pedro <75582165+joao-AR@users.noreply.github.com>
2022-02-04 15:36:00 -0300

    Delete node directory

--- node/outros/moduloA.js
+++
@@ -1,3 +0,0 @@
-this.ola = 'fala pessoal'
-exports.bemVindo = 'Bem vindo ao node!'
-module.exports.atelogo = 'até logo'
--- node/outros/usandoModulosTerceiros.js
+++
@@ -1,3 +0,0 @@
-const _= require('lodash')
-
-setInterval(() => console.log (_.random(1,100)),2000)
