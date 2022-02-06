96d4348bcbd1de509f61cd9b56369cbf89fee89f
Joao <jp.alvesrodrigues02@gmail.com>
2022-02-06 16:24:37 -0300

    add HTML

---
+++ 2_HTML/exercicios/form/server.js
@@ -0,0 +1,23 @@
+
+const express = require('express')
+const app = express()
+const bodyParser= require('body-parser')
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.post('/usuarios',(req,resp)=>{
+    console.log(req.body)
+    resp.send('<h1>Parabéns. Usuario incluido</h1>')
+})
+
+app.post('/usuarios/:id',(req,resp)=>{
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Parabéns. Usuario Alterado!!</h1>')
+})
+
+
+app.listen(4004)
+
